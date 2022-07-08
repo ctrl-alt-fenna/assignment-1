44eede92e3ed5651e4dad755aff2449dc4ce6d34
Fenna <ikbenfenna@gmail.com>
2022-07-08 20:30:02 +0200

    MOD: Added way to buy laptops

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 let laptopCollection = [];
+const salary = 100
+const URL = 'https://noroff-komputer-store-api.herokuapp.com/'
+
 const workBtn = document.getElementById('work-pay')
 const bankBtn = document.getElementById('work-bank')
 const loanBtn = document.getElementById('loan')
-const salary = 100
+const buyBtn = document.getElementById('buy')
 
 let selMenu = document.getElementById('laptops')
 let payLoanBtn = document.getElementById('pay-loan')
+
 let loanBal = document.getElementById('loan-bal')
 let outLoan = document.getElementById('out-loan')
 let balance = document.getElementById('bal')
+let stockCount = document.getElementById('stock')
 let pay = document.getElementById('pay')
 let lapSpec = document.getElementById('lap-spec')
 let lapDesc = document.getElementById('lap-desc')
@@ -17,13 +22,13 @@ let lapMain = document.getElementById('lap-main')
 let currLoan = Number(loanBal.innerHTML)
 let currBalance = Number(balance.innerHTML)
 let currPay = Number(balance.innerHTML)
-
+let currID = 0
 let hasLoan = false
 
-outLoan.style.visibility = "hidden"
-payLoanBtn.style.visibility = "hidden"
+outLoan.style.visibility = 'hidden'
+payLoanBtn.style.visibility = 'hidden'
 
-getLaptops();
+parseLaptops();
 
 loanBtn.onclick = () => {
     // First check if there is already a loan out
@@ -52,6 +57,52 @@ payLoanBtn.onclick = () => {
 selMenu.onchange = (e) => {
     changeSpecs(e.target.value - 1)
 }
+buyBtn.onclick = () => {
+    buyLaptop()
+}
+
+function parseLaptops() {
+    var xhttp = new XMLHttpRequest();
+    let laptopItems;
+    xhttp.onload = () => {
+        if (xhttp.readyState == 4) {
+            laptopItems = JSON.parse(xhttp.response)
+            createOptions(laptopItems)
+        }
+    }
+    xhttp.open('GET', URL + "computers")
+    xhttp.send()
+}
+
+function createOptions(laptopItems) {
+    for (const iterator of laptopItems) {
+        let optNode = document.createElement('option')
+        optNode.value = iterator.id;
+        optNode.text = iterator.title;
+        selMenu.appendChild(optNode)
+        laptopCollection.push(iterator);
+    }
+    changeSpecs()
+}
+
+function changeSpecs(id = currID) {
+    lapSpec.textContent = ' '
+    let currLaptop = laptopCollection[id]
+    currLaptop.specs.map((e) => {
+        let textItem = document.createElement('li')
+        textItem.textContent = e
+        lapSpec.appendChild(textItem)
+    })
+    let lapTitle = document.getElementById('lap-title')
+    let lapImg = document.getElementById('lap-img')
+    let lapPrice = document.getElementById('price')
+    lapPrice.textContent = currLaptop.price;
+    lapTitle.textContent = currLaptop.title
+    lapDesc.textContent = currLaptop.description;
+    stockCount.innerHTML = currLaptop.stock;
+    lapImg.src = URL + currLaptop.image
+    currID = id
+}
 
 function updateLoan(amount) {
     if (hasLoan) {
@@ -63,9 +114,8 @@ function updateLoan(amount) {
             hasLoan = false
             amount -= currLoan
             currLoan = 0
-            outLoan.style.visibility = "hidden"
-            payLoanBtn.style.visibility = "hidden"
-            outLoan.innerHTML = currLoan
+            outLoan.style.visibility = 'hidden'
+            payLoanBtn.style.visibility = 'hidden'
             updateBalance(amount);
         }
     }
@@ -76,10 +126,11 @@ function updateLoan(amount) {
         // If the user has an outstanding loan, do not allow a second loan to be taken
     else {
         hasLoan = true
-        currLoan = Number(loanBal.innerHTML) + Number(amount)
+        currLoan = Number(amount)
+        console.log(currLoan)
         loanBal.innerHTML = currLoan
-        payLoanBtn.style.visibility = "visible"
-        outLoan.style.visibility = "visible"
+        payLoanBtn.style.visibility = 'visible'
+        outLoan.style.visibility = 'visible'
         updateBalance(amount)
     }
 }
@@ -100,42 +151,13 @@ function transferMoney(amount) {
 }
 
 
-function getLaptops() {
-    var xhttp = new XMLHttpRequest();
-    let laptopOptions;
-    xhttp.onload = () => {
-        if (xhttp.readyState == 4) {
-            laptopOptions = JSON.parse(xhttp.response)
-            createOptions(laptopOptions)
-        }
+function buyLaptop() {
+    if (Number(stockCount.innerHTML) == 0) alert('Laptop already sold!')
+    else if (laptopCollection[currID].price > Number(balance.innerHTML)) alert('Insufficient funds')
+    else {
+        alert('Laptop bought!')
+        currBalance = Number(balance.innerHTML) - laptopCollection[currID].price;
+        balance.innerHTML = currBalance
+        stockCount.innerHTML = Number(stockCount.innerHTML) - 1
     }
-    xhttp.open('GET', 'https://noroff-komputer-store-api.herokuapp.com/computers')
-    xhttp.send()
-}
-
-function createOptions(laptopOptions) {
-    for (const iterator of laptopOptions) {
-        let optNode = document.createElement('option')
-        optNode.value = iterator.id;
-        optNode.text = iterator.title;
-        selMenu.appendChild(optNode)
-        laptopCollection.push(iterator);
-    }
-    changeSpecs(0)
-}
-
-function changeSpecs(id) {
-    lapSpec.textContent = ' '
-    laptopCollection[id].specs.map((e) => {
-        let textItem = document.createElement('li')
-        textItem.textContent = e
-        lapSpec.appendChild(textItem)
-    })
-    let lapTitle = document.getElementById('lap-title')
-    let lapImg = document.getElementById('lap-img')
-    let lapPrice = document.getElementById('price')
-    lapPrice.textContent = laptopCollection[id].price;
-    lapTitle.textContent = laptopCollection[id].title
-    lapDesc.textContent = laptopCollection[id].description;
-    lapImg.src = 'https://noroff-komputer-store-api.herokuapp.com/' + laptopCollection[id].image
 }
